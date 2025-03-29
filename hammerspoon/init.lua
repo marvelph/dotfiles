a53ad09150f58ea1476c36c032c21b8461cf1d34
marvelph <marvel@programmershigh.org>
2025-03-29 12:49:09 +0900

    Update configuration file

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,106 +1,46 @@
-function chooserCompletion(choice)
-    if choice then
-        if choice.workspaceName then
-            hs.execute("/opt/homebrew/bin/aerospace workspace " .. choice.workspaceName)
-        elseif choice.windowId then
-            hs.execute("/opt/homebrew/bin/aerospace focus --window-id " .. choice.windowId)
-        end
-    end
-end
+PaperWM = hs.loadSpoon("PaperWM")
+PaperWM:bindHotkeys({
+    refresh_windows      = { { "cmd" }, "forwarddelete" },
+    toggle_floating      = { { "cmd" }, "padclear" },
+    focus_left           = { { "cmd" }, "left" },
+    focus_right          = { { "cmd" }, "right" },
+    focus_up             = { { "cmd" }, "up" },
+    focus_down           = { { "cmd" }, "down" },
+    swap_left            = { { "alt", "cmd" }, "left" },
+    swap_right           = { { "alt", "cmd" }, "right" },
+    swap_up              = { { "alt", "cmd" }, "up" },
+    swap_down            = { { "alt", "cmd" }, "down" },
+    center_window        = { { "cmd" }, "padenter" },
+    full_width           = { { "cmd" }, "pad=" },
+    cycle_width          = { { "cmd" }, "pad+" },
+    cycle_height         = { { "cmd" }, "pad-" },
+    reverse_cycle_width  = { { "alt", "cmd" }, "pad+" },
+    reverse_cycle_height = { { "alt", "cmd" }, "pad-" },
+    slurp_in             = { { "cmd" }, "pad*" },
+    barf_out             = { { "cmd" }, "pad/" },
+    switch_space_l       = { { "cmd" }, "," },
+    switch_space_r       = { { "cmd" }, "pad." },
+    switch_space_1       = { { "cmd" }, "pad1" },
+    switch_space_2       = { { "cmd" }, "pad2" },
+    switch_space_3       = { { "cmd" }, "pad3" },
+    switch_space_4       = { { "cmd" }, "pad4" },
+    switch_space_5       = { { "cmd" }, "pad5" },
+    switch_space_6       = { { "cmd" }, "pad6" },
+    switch_space_7       = { { "cmd" }, "pad7" },
+    switch_space_8       = { { "cmd" }, "pad8" },
+    switch_space_9       = { { "cmd" }, "pad9" },
+    move_window_1        = { { "alt", "cmd" }, "pad1" },
+    move_window_2        = { { "alt", "cmd" }, "pad2" },
+    move_window_3        = { { "alt", "cmd" }, "pad3" },
+    move_window_4        = { { "alt", "cmd" }, "pad4" },
+    move_window_5        = { { "alt", "cmd" }, "pad5" },
+    move_window_6        = { { "alt", "cmd" }, "pad6" },
+    move_window_7        = { { "alt", "cmd" }, "pad7" },
+    move_window_8        = { { "alt", "cmd" }, "pad8" },
+    move_window_9        = { { "alt", "cmd" }, "pad9" }
+})
+PaperWM.window_filter:rejectApp("ClipBook")
+PaperWM:start()
 
-chooser = hs.chooser.new(chooserCompletion)
-
-function selectWorkspace()
-    chooser:cancel()
-
-    local output, status = hs.execute("/opt/homebrew/bin/aerospace list-workspaces --monitor focused --empty no --json")
-    if not status then
-        return
-    end
-    local workspacesJson = hs.json.decode(output)
-    local choices = {}
-    for _, workspaceJson in ipairs(workspacesJson) do
-        local output, status = hs.execute("/opt/homebrew/bin/aerospace list-windows --workspace " .. workspaceJson["workspace"] .. " --format %{app-bundle-id}%{app-name} --json")
-        if not status then
-            return
-        end
-        local windowsJson = hs.json.decode(output)
-        local subText = nil
-        local image = nil
-        for _, windowJson in ipairs(windowsJson) do
-            if not subText then
-                subText = windowJson["app-name"]
-                image = hs.image.imageFromAppBundle(windowJson["app-bundle-id"])
-            else
-                subText = subText .. ", " .. windowJson["app-name"]
-            end
-        end
-        local choice = {
-            text = "workspace " .. workspaceJson["workspace"],
-            subText = subText,
-            image = image,
-            workspaceName = workspaceJson["workspace"]
-        }
-        table.insert(choices, choice)
-    end
-    chooser:choices(choices)
-
-    chooser:show()
-end
-
-function selectWindow()
-    chooser:cancel()
-
-    local output, status = hs.execute("/opt/homebrew/bin/aerospace list-windows --workspace focused --format %{window-id}%{window-title}%{app-bundle-id}%{app-name} --json")
-    if not status then
-        return
-    end
-    local windowsJson = hs.json.decode(output)
-    local choices = {}
-    for _, windowJson in ipairs(windowsJson) do
-        local choice = {
-            text = windowJson["window-title"],
-            subText = windowJson["app-name"],
-            image = hs.image.imageFromAppBundle(windowJson["app-bundle-id"]),
-            windowId = windowJson["window-id"]
-        }
-        table.insert(choices, choice)
-    end
-    chooser:choices(choices)
-
-    chooser:show()
-end
-
-function updateMenubar(workspaceName)
-    local output, status = hs.execute("/opt/homebrew/bin/aerospace list-workspaces --monitor focused --json")
-    if not status then
-        return
-    end
-    local workspacesJson = hs.json.decode(output)
-    local title = nil
-    for _, workspaceJson in ipairs(workspacesJson) do
-        local workspaceTitle = workspaceJson["workspace"]
-        if workspaceTitle == workspaceName then
-            workspaceTitle = "[ " .. workspaceTitle .. " ]"
-        end
-        if not title then
-            title = workspaceTitle
-        else
-            title = title .. " " .. workspaceTitle
-        end
-    end
-    menubar:setTitle(title)
-end
-
-menubar = hs.menubar.new()
-updateMenubar(nil)
-
-function onWorkspaceChange(eventName, params)
-    updateMenubar(params["focused-workspace"])
-
-    hs.alert.show("workspace " .. params["focused-workspace"])
-end
-
-hs.hotkey.bind({ "alt", "cmd" }, "up", selectWorkspace)
-hs.hotkey.bind({ "alt", "cmd" }, "down", selectWindow)
-hs.urlevent.bind("on-workspace-change", onWorkspaceChange)
+ActiveSpace = hs.loadSpoon("ActiveSpace")
+ActiveSpace:start()
